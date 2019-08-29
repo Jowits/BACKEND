ce0f1e848ea8529fe7a2ac2fa5001dcc78091317
Jowita <jowita.lapies@gmail.com>
2019-08-29 08:07:56 +0100

    changed password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
 
 
     has_secure_password
-    validates :username, presence: true
-    validates :username, uniqueness: true
+    validates :username, presence: true, uniqueness: true
 end
